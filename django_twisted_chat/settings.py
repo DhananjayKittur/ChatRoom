37db1b3a284ce6f1c61637813f0db5ba70c5d968
DhananjayKittur <danukg.sjce@gmail.com>
2017-03-31 16:21:25 +0200

    commit

---
+++ django_twisted_chat/settings.py
@@ -0,0 +1,132 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+   
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': './sqlite_database_file',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
+
+
+ALLOWED_HOSTS = []
+
+
+TIME_ZONE = 'America/Chicago'
+
+
+LANGUAGE_CODE = 'en-us'
+
+SITE_ID = 1
+
+USE_I18N = True
+
+USE_L10N = True
+
+
+USE_TZ = True
+
+
+MEDIA_ROOT = ''
+
+
+MEDIA_URL = ''
+
+
+STATIC_ROOT = ''
+
+
+STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = (
+
+    os.path.join(BASE_DIR, "static"),
+)
+
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+
+)
+
+
+SECRET_KEY = '%w0lfyrrsax9+^@5s9jo#$o#pi+3#n4qkn!k5lpq7hucsg@1@&'
+
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+ROOT_URLCONF = 'django_twisted_chat.urls'
+
+
+WSGI_APPLICATION = 'django_twisted_chat.wsgi.application'
+
+TEMPLATE_DIRS = (
+
+    os.path.join(BASE_DIR, 'templates'),
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+    'chat',
+)
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
